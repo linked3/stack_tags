5aeb833678bb5fcb55de253f98872a130684e25e
linked3 <148115250+linked3@users.noreply.github.com>
2025-12-26 11:07:48 +0100

    backport to 1.18.2

--- common/src/main/java/net/linkedto/stack_tags/mixin/MixinItemStack.java
+++ common/src/main/java/net/linkedto/stack_tags/mixin/MixinItemStack.java
@@ -1,6 +1,6 @@
 package net.linkedto.stack_tags.mixin;
 
-import net.minecraft.core.registries.Registries;
+import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
@@ -12,13 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemStack.class)
 public class MixinItemStack {
-    private static final TagKey<Item> STACKABLE_TO_1 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_1"));
-    private static final TagKey<Item> STACKABLE_TO_2 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_2"));
-    private static final TagKey<Item> STACKABLE_TO_4 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_4"));
-    private static final TagKey<Item> STACKABLE_TO_8 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_8"));
-    private static final TagKey<Item> STACKABLE_TO_16 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_16"));
-    private static final TagKey<Item> STACKABLE_TO_32 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_32"));
-    private static final TagKey<Item> STACKABLE_TO_64 = TagKey.create(Registries.ITEM, new ResourceLocation("stack_tags", "stackable_to_64"));
+    private static final TagKey<Item> STACKABLE_TO_1 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_1"));
+    private static final TagKey<Item> STACKABLE_TO_2 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_2"));
+    private static final TagKey<Item> STACKABLE_TO_4 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_4"));
+    private static final TagKey<Item> STACKABLE_TO_8 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_8"));
+    private static final TagKey<Item> STACKABLE_TO_16 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_16"));
+    private static final TagKey<Item> STACKABLE_TO_32 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_32"));
+    private static final TagKey<Item> STACKABLE_TO_64 = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("stack_tags", "stackable_to_64"));
 
     @Inject(method = "getMaxStackSize()I", at = @At("RETURN"), cancellable = true)
     private void examplemod$adjustMaxStackSize(CallbackInfoReturnable<Integer> cir) {
